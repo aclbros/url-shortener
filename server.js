4194491b36f63d5276ccab44b9db4e0c22381e8b
Antonio Carretero <aclbros12@yahoo.es>
2016-07-04 09:29:42 +0000

    first commit

--- server.js
+++ server.js
@@ -1,32 +1,15 @@
-'use strict';
-
-var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
+var express = require("express");
+var api = require("./api.js");
 
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
+require("dotenv").load();
 
-mongoose.connect(process.env.MONGO_URI);
+app.set('view engine', 'ejs');
 
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
+app.use('/css', express.static(__dirname + '/views/css'));
+app.use(api);
 
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
-
+// start the server
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
